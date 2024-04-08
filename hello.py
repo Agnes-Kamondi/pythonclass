6951c34c9f4f06e7a964a9bc30fd171fb47ef52e
Agnes-Kamondi <kamondiagnesauma@gmail.com>
2024-04-08 15:49:39 +0300

    ...

--- hello.py
+++ hello.py
@@ -1,8 +1,17 @@
 def hello():
     print("Hello Akirachix")
 
+hello()
+
 def hello(name):
     print(f"Hello{name}")
+hello("Agnes")
+hello("Yvonne")
 
 def year_of_birth(name,age):     #the def should be at the geginning of a new line, and the indentation is in the right place
     print(f"Hello {name}, you were born in {2024-age}")
+
+year_of_birth("Agnes",21)
+year_of_birth("Yvonne",24)
+year_of_birth("Martha",22)
+
